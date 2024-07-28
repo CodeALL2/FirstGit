9d25fe8e1aae962a2a0d73869e73632f8c12d104
codebo <2653400637@qq.com>
2024-07-28 23:14:56 +0800

    No104

---
+++ No104/Solution.java
@@ -0,0 +1,28 @@
+package No104;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        if (root.left == null && root.right == null){
+            return 1;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return left > right ? left + 1 : right+1;
+    }
+}
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
